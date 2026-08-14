010adbb4218fca10eea9c0a9b8eb555416f19d38
hussainbinfazal <mdhussain0610@gmail.com>
2026-08-14 22:54:47 +0530

    Added ngnix.conf with api route to match the route

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const axiosInstance = axios.create({
-  baseURL:import.meta.env.VITE_AXIOS_URL ? import.meta.env.VITE_AXIOS_URL  : 'http://localhost:5000/api',
+  baseURL: '/api',
   withCredentials: true,
 });
 
